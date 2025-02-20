2bc49f26985be9ca3165e1ad52e3b54b7fafb74b
Kareem Ahmad <zadkareem@gmail.com>
2025-02-20 23:28:00 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_URL || '/GLUE/',
+  base: process.env.BASE_URL || '/Glue/',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
