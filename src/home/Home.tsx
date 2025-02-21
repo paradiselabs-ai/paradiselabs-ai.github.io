4573713d8b4ea01a9665172a85a517aeb44494bb
Kareem Ahmad <zadkareem@gmail.com>
2025-02-22 01:13:57 +0200

    update

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -42,10 +42,7 @@ const GradientBackground = memo(() => (
       </defs>
     </svg>
     <div className="gradients-container">
-      <div className="g1"></div>
-      <div className="g2"></div>
-      <div className="g3"></div>
-      <div className="g4"></div>
+
     </div>
   </div>
 ));
