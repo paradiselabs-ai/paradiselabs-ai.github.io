2b0c9e255ec564a302ddb1a37924c8cd71191276
Kareem Ahmad <zadkareem@gmail.com>
2025-03-17 16:54:48 +0200

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const AppContent: React.FC = () => {
       <HashRouter>
         <a 
           href="#main-content" 
-          className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-white focus:text-black focus:outline focus:outline-2"
+          className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-white focus:text-black focus:outline-2"
           style={{ clip: 'rect(0, 0, 0, 0)', clipPath: 'inset(50%)', overflow: 'hidden', position: 'absolute', whiteSpace: 'nowrap', width: '1px', height: '1px' }}
         >
           Skip to main content
